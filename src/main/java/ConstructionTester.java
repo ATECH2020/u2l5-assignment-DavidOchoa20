85c0400b18624e431b1a47ea865b0f90c08f869b
David Ochoa <david.232954@nv.ccsd.net>
2020-09-23 20:30:42 +0000

    draft 1

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,22 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
-    }
-}
+      Scanner in = new Scanner(System.in);     
+      double lumCost = 8;
+      double winCost = 11;
+      double taxRate, total, grandTotal;
+      int numBoard, numWin;
+
+      System.out.println("Enter the sales tax rate: ");
+      taxRate = in.nextDouble();
+      System.out.println("How many boards do you need?");
+      numBoard = in.nextInt();
+      System.out.println("How many windows do you need?");
+      numWin = in.nextInt();
+      Construction test =  new Construction(lumCost, winCost, taxRate);
+      total = test.lumberCost(numBoard) + test.windowCost(numWin);
+      System.out.println("Total: " + total);
+      grandTotal = test.grandTotal(total);
+      System.out.println("Grand Total: " + grandTotal);
+    }//end main method
+}//end class
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,25 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
-    }
-}
+      double startLat, startLong, endLat, endLong, distance;
+
+      //User Input
+      Scanner in = new Scanner(System.in);  
+      System.out.print("Enter the latitude of the starting location: "); 
+      startLat = in.nextDouble();
+      System.out.print("Enter the longitude of the starting location: ");
+      startLong = in.nextDouble();
+      System.out.print("Enter the latitude of the ending location: "); 
+      endLat = in.nextDouble();
+      System.out.print("Enter the longitude of the ending location: ");
+      endLong = in.nextDouble();
+
+      //creates start and end GeoLocation
+      GeoLocation startLoc = new GeoLocation(startLat, startLong);
+      GeoLocation endLoc = new GeoLocation(endLat, endLong);
+
+      //Calculations
+      distance = startLoc.distanceFrom(endLoc);
+      System.out.print("The distance is " + distance + " miles.");
+    }//end main method
+}//end class
